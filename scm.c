104474feee259825100915627a34d75e0d9c50d0
unknown <igni@cin.ufpe.br>
2025-02-19 15:13:35 -0300

    adicionando funcao divide

--- scm.c
+++ scm.c
@@ -8,9 +8,20 @@ void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
+void divide(int a, int b) {
+    if (b==0){
+    printf("divisão não permitida\n");
+    }
+    else{
+    printf("%d / %d = %d\n", a, b, a / b);
+    }
+}
+
 int main() {
     printf("Olá, Estudante!\n");
     soma(5, 3);
     multiplica(5, 3);
+    divide(9, 3);
+    divide(5, 0);
     return 0;
 }
