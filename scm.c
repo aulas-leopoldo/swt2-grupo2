e887d8d01a5eaa8b4dd41a9b2c79d10a6b74fe5b
José Aurélio Carvalho <joseaurelio.99.jaec@gmail.com>
2025-02-19 15:11:33 -0300

    Added getTimeAndDate function

--- scm.c
+++ scm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 void soma(int a, int b) {
     printf("%d + %d = %d\n", a, b, a + b);
@@ -8,8 +9,14 @@ void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
+char * getTimeAndDate(){
+    time_t t = time(NULL);
+    return ctime(&t);
+}
+
+
 int main() {
-    printf("Olá, Estudante!\n");
+    printf("Ola, Estudante! - %s\n", getTimeAndDate());
     soma(5, 3);
     multiplica(5, 3);
     return 0;
