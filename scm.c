1a421ca97080e86f216f082069946d3be12c123f
Luan Galindo <132147646+luangalindo1@users.noreply.github.com>
2025-02-19 15:10:56 -0300

    remoção da função multiplica

--- scm.c
+++ scm.c
@@ -4,9 +4,10 @@ void soma(int a, int b) {
     printf("%d + %d = %d\n", a, b, a + b);
 }
 
-void multiplica(int a, int b) {
+// MODF Luan: Remover função multiplica
+/*void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
-}
+}*/
 
 void subtrai(int a, int b){
     if(a-b >= 0)
