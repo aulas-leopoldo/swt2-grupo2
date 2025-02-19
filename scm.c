bc2791db7996cf2cd58db36977e4e2933c33a251
Juliane <juliane.coutinho734@gmail.com>
2025-02-19 15:07:55 -0300

    Adicionando funcao subtrai

--- scm.c
+++ scm.c
@@ -8,9 +8,15 @@ void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
+void subtrai(int a, int b){
+    if(a-b >= 0)
+        printf("%d - %d = %d\n", a, b, a-b);
+}
+
 int main() {
     printf("Olá, Estudante!\n");
     soma(5, 3);
     multiplica(5, 3);
+    subtrai(5,3);
     return 0;
 }
